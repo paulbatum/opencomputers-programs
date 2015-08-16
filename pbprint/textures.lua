811d1befd93bf3256d8f2793f332f14e06ae27d7
Paul Batum <paul.batum@gmail.com>
2015-08-15 20:16:15 -0700

    add textures

--- pbprint/textures.lua
+++ pbprint/textures.lua
@@ -2,6 +2,9 @@ local textures = {}
 
 textures = {  
   tyrian_dent = "chisel:tyrian/dent",  
+  tyrian_tiles = "chisel:tyrian/platetiles",
+  diamond_obsidian = "chisel:diamond/terrain-diamond-space-side",
+  nullifier = "thermalexpansions:device/Device_Active_Nullifier",
   ender_flow = "thermalfoundation:fluid/Fluid_Ender_Flow",
   redstone_flow = "thermalfoundation:fluid/Fluid_Redstone_Flow",
   redstone_still = "thermalfoundation:fluid/Fluid_Redstone_Still",
